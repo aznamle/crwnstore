ffdd2feaaf5df80a1cb79bd7e5555f17554b207e
Nam Le <kimlonglecis@gmail.com>
2020-10-01 16:04:53 -0700

    added header component and links to home, shop, contact

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 
 
 function App() {
   return (
       <div>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
       </div>
   );
 }
